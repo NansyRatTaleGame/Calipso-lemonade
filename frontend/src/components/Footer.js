3764a980faf2c828132d6a15c1b8adf72227bf35
Anastasy <NansyPanniman21@gmail.com>
2024-12-22 19:22:52 +0300

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="Footer flex flex-col">
+      <div className="container p-4">
+        <span>&copy;2024 ООО &laquo;Calypso&raquo;</span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
